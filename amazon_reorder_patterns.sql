e207e62cb3538eb3bf648ecbe0abb81b75a77342
AlexOchoki <106147873+AlexOchoki@users.noreply.github.com>
2026-02-08 23:14:52 +0300

    Amazon reorder patterns

---
+++ amazon_reorder_patterns.sql
@@ -0,0 +1,18 @@
+-- Q1
+-- To better understand customer preferences, the team needs to know the details of customers who reorder specific products. 
+-- Can you retrieve the customer information along with their reordered product code(s) for Q4 2024?
+
+-- fct_orders(order_id, customer_id, product_id, reorder_flag, order_date)
+-- dim_products(product_id, product_code, category)
+-- dim_customers(customer_id, customer_name)
+
+
+
+SELECT   LEFT(p.product_code, 3) AS Product_Cdode,
+         COUNT(f.reorder_flag) AS re_order_flag
+FROM fct_orders f
+JOIN dim_products p 
+ON p.product_id = f.product_id
+WHERE f.reorder_flag = '1'
+AND f.order_date BETWEEN '2024-10-01' AND '2024-12-31'
+GROUP BY LEFT(p.product_code, 3)
