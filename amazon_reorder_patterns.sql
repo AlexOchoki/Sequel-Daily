e84d9fa1044f7b5f6dbab0eb40d12598b25b91fd
AlexOchoki <106147873+AlexOchoki@users.noreply.github.com>
2026-02-09 07:58:30 +0300

    Customer product preferences with their info

--- amazon_reorder_patterns.sql
+++ amazon_reorder_patterns.sql
@@ -16,3 +16,22 @@ ON p.product_id = f.product_id
 WHERE f.reorder_flag = '1'
 AND f.order_date BETWEEN '2024-10-01' AND '2024-12-31'
 GROUP BY LEFT(p.product_code, 3)
+
+
+
+
+-- q2
+-- To better understand customer preferences, the team needs to know the details of customers who reorder specific products. 
+-- Can you retrieve the customer information along with their reordered product code(s) for Q4 2024?
+
+SELECT  c.customer_name,
+         p.product_code,
+         COUNT(o.product_id) AS order_count
+  FROM fct_orders o 
+JOIN dim_customers c 
+ON o.customer_id = c.customer_id
+JOIN dim_products p 
+ON o.product_id = p.product_id
+WHERE reorder_flag = '1'
+AND o.order_date BETWEEN '2024-10-01' AND '2024-12-31'
+GROUP BY c.customer_name, p.product_code;
