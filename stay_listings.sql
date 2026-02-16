dbab4cf28dba8c7bb6d4075efde559f9e1b89425
Alex Mogengo <106147873+AlexOchoki@users.noreply.github.com>
2026-02-16 23:01:57 +0300

    Add query for average nightly price difference
    Add SQL query to calculate average price difference between listings with and without cleaning fees for July 2024.

--- stay_listings.sql
+++ stay_listings.sql
@@ -13,3 +13,19 @@ ON b.listing_id = l.listing_id
 WHERE b.booking_date >= '2024-07-01' AND b.booking_date < '2024-08-01'
 AND b.booked_nights >= 1
 AND (l.amenities LIKE '%pool%' OR l.amenities LIKE '%ocean view%');
+
+--Q2
+--For listings with no cleaning fee (i.e., NULL values in the 'cleaning_fee' column), 
+--what is the average difference in nightly price compared to listings with a cleaning fee in July 2024?
+
+SELECT 
+    AVG(CASE WHEN cleaning_fee IS NULL 
+             THEN nightly_price END)
+  -
+    AVG(CASE WHEN cleaning_fee IS NOT NULL 
+             THEN nightly_price END)
+    AS avg_price_difference
+FROM fct_bookings
+WHERE booking_date >= '2024-07-01'
+  AND booking_date <  '2024-08-01'
+  AND booked_nights >= 1;
